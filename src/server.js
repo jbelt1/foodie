79a6c8a48b217f3420314e52ae68d05389307352
Kevin Aranyi <karanyi@terpmail.umd.edu>
2018-04-07 14:55:36 -0400

    Api Call

--- src/server.js
+++ src/server.js
@@ -1,5 +1,77 @@
-var http = require('http');
+var express = require('express');
+var app = express();
+const yelp = require('yelp-fusion');
+const url = require('url'); 
+const querystring = require('querystring'); 
 
-http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-}).listen(8080);
+const apiKey = 'NEuYDf8DITFJd77D9sVOsZDNNoQLC7YK4F3v1o_ZLTqIbB7RAcelzPsoycXetJOig1A125Cjrns0edw03oTSxcUEImaj_6qAuq4pyRfKm9SxrreVEqn8yJTDNEbIWnYx';
+
+const client = yelp.client(apiKey);
+
+const prices = {"$": 1, "$$": 2, "$$$": 3, "$$$$": 4};
+
+const undefinedJSON = {
+	isFound: false,
+	errorMessage: "There are no results that meet your criteria"
+}
+
+app.get('/', function(req, res){
+	var areaUnderscores = req.query.location;
+	var food = req.query.food;
+	var inputBudget = req.query.budget;
+	var budget;
+	var found = false;
+
+	var area = areaUnderscores.replace(/_/g, " ");
+	budget = parseInt(inputBudget);
+
+	var searchRequest = {
+		term: food,
+		location: area
+	};
+
+	console.log(food);
+	console.log(area);
+
+	if (budget < 8) {
+		budget = 1;
+	} else if (budget < 12) {
+		budget = 2;
+	} else if (budget < 16) {
+		budget = 3;
+	} else {
+		budget = 4;
+	}
+
+	console.log(budget);
+
+	client.search(searchRequest).then(response => {
+		  var i = 0;
+		  var currentBusiness;
+		  var businesses = response.jsonBody.businesses;
+		  var Result;
+		  
+		  while (!found && i < businesses.length) {
+		  	currentBusiness = businesses[i];
+		  	if (prices[currentBusiness.price] < budget) {
+		  		Result = currentBusiness;
+		  		Result["isFound"] = true;
+		  		Result["errorMessage"] = null;
+		  		found = true;
+		  	}
+		  	i++;
+		  }
+
+		  if (!found) {
+		  	res.send(undefinedJSON);
+		  } else {
+		  	res.send(Result);
+		  }
+
+		}).catch(e => {
+		  console.log(e);
+		});
+
+});
+
+app.listen(8080);
