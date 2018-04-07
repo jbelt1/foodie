582c733a30d78597935f9101d92f0c7c76afc9a0
Jeffrey Belt III <jbelt1@umd.edu>
2018-04-07 14:51:50 -0400

    Adds FoodFinder UI functionality

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './css/index.css';
+import FoodieApp from './components/FoodieApp';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<FoodieApp />, document.getElementById('root'));
 registerServiceWorker();
